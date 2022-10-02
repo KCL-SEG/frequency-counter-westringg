f86c1e2ae715c41f40239f5d314d24592acd3e66
SeoHyun Kwon <seohyun.kwon@kcl.ac.uk>
2022-10-02 21:54:51 +0100

    Add the functionality of returning the dictionary of  frequencies

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,21 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in range(len(items)):
+        items[i] = str(items[i])
+    print(items)
+
+    for item in items:
+        isExist = False
+        for key in frequencies.keys():
+            if item == key:
+                isExist = True
+                curCount = frequencies[item]
+                frequencies.update({item:curCount+1})
+                #print('curCount', curCount)
+        if not isExist:
+            frequencies.update({item:1})
+        
     return frequencies
+
+#print(frequencies([100, 'Hello', '100', '100', 100]))
